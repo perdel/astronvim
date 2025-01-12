7fdd85396435c4ddc543f1c4d140b4cdeb04b0d4
Delwin Perera <perera@mm.tu-darmstadt.de>
2025-01-12 01:49:50 +0100

    Define treesitter language plugins
    Signed-off-by: Delwin Perera <perera@mm.tu-darmstadt.de>

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- Customize Treesitter
 
 ---@type LazySpec
@@ -10,6 +8,8 @@ return {
       "python",
       "lua",
       "vim",
+      "regex",
+      "bash",
       -- add more arguments for adding more treesitter parsers
     },
   },
