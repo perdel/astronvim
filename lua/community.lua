9f4eea347e8dd93d85fc61799508ff23b0671e2b
Delwin Perera <perera@mm.tu-darmstadt.de>
2025-06-14 23:16:11 +0200

    Adapt status line
    Signed-off-by: Delwin Perera <perera@mm.tu-darmstadt.de>

--- lua/community.lua
+++ lua/community.lua
@@ -11,5 +11,6 @@ return {
   { import = "astrocommunity.workflow.hardtime-nvim" },
   { import = "astrocommunity.utility.noice-nvim" },
   { import = "astrocommunity.recipes.heirline-vscode-winbar" },
+  { import = "astrocommunity.recipes.heirline-nvchad-statusline" },
   -- import/override with your plugins folder
 }
